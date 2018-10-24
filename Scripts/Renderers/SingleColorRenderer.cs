9fa85aa00ca66d4070dc77aa46683d11873286c1
mark <mark.korput@gmail.com>
2018-10-24 11:41:20 +0200

    Proper CPU-based triangle mesh subdiviver

--- Scripts/Renderers/SingleColorRenderer.cs
+++ Scripts/Renderers/SingleColorRenderer.cs
@@ -5,39 +5,39 @@ using UnityEngine;
 namespace Computils.Renderers
 {
 	[AddComponentMenu("Computils/Renderers/Single Color Renderer")]
-    public class SingleColorRenderer : MonoBehaviour
-    {
-        private static class ShaderProps
-        {
-            public const string buf_verts = "buf_verts";
+	public class SingleColorRenderer : MonoBehaviour
+	{
+		private static class ShaderProps
+		{
+			public const string buf_verts = "buf_verts";
 			public const string buf_alphafactors = "buf_alphafactors";
 			public const string MainColor = "MainColor";
 			public const string UseAlphaFactors = "UseAlphaFactors";
-        }
-      
-        public ComputeBufferFacade VertsBufFacade;
+		}
+
+		public ComputeBufferFacade VertsBufFacade;
 		[Tooltip("Optional; when set, it will use these factors as alpha multiplier")]
 		public ComputeBufferFacade AlphaFactorsFacade;
-        public Material RenderMaterial;
-        public MeshTopology MeshTopology = MeshTopology.Points;
-        public Color MainColor = new Color(1, 1, 1, 0.3f);
+		public Material RenderMaterial;
+		public MeshTopology MeshTopology = MeshTopology.Points;
+		public Color MainColor = new Color(1, 1, 1, 0.3f);
 
 #if UNITY_EDITOR
-        [Header("Read-Only")]
-        public int VertCount = 0;
+		[Header("Read-Only")]
+		public int VertCount = 0;
 #endif
 
 		private void Start()
 		{
 			this.RenderMaterial = new Material(this.RenderMaterial);
 		}
-      
+
 		//After all rendering is complete we dispatch the compute shader and then set the material before drawing with DrawProcedural
 		//this just draws the "mesh" as a set of points
 		void OnPostRender()
-        {
+		{
 			var buf = this.VertsBufFacade.GetValid();
-         
+
 			if (buf != null)
 			{
 				Render(this.RenderMaterial, buf, this.MeshTopology, this.MainColor, this.AlphaFactorsFacade == null ? null : this.AlphaFactorsFacade.GetValid());
@@ -45,17 +45,38 @@ namespace Computils.Renderers
 				this.VertCount = buf.count;
 #endif
 			}
-        }
-      
+		}
+
 		private static void Render(Material mat, ComputeBuffer vertsBuffer, MeshTopology topo, Color clr, ComputeBuffer alphaFactorsBuf = null)
-        {
-            mat.SetPass(0);
+		{
+			mat.SetPass(0);
 			mat.SetBuffer(ShaderProps.buf_verts, vertsBuffer);
 			mat.SetColor(ShaderProps.MainColor, clr);
 			mat.SetInt(ShaderProps.UseAlphaFactors, alphaFactorsBuf == null ? 0 : 1);
 			if (alphaFactorsBuf != null) mat.SetBuffer(ShaderProps.buf_alphafactors, alphaFactorsBuf);
 
-            Graphics.DrawProcedural(topo, vertsBuffer.count);
-        }
-    }
+			Graphics.DrawProcedural(topo, vertsBuffer.count);
+		}
+
+		#region Public Methods
+		public void CycleTopology() {
+			MeshTopology topo = MeshTopology.Lines;
+         
+			switch (this.MeshTopology) {
+				// case MeshTopology.Points: topo = MeshTopology.Lines; break;
+				case MeshTopology.Lines: topo = MeshTopology.LineStrip; break;
+				case MeshTopology.LineStrip: topo = MeshTopology.Triangles; break;
+				case MeshTopology.Triangles: topo = MeshTopology.Quads; break;
+				case MeshTopology.Quads: topo = MeshTopology.Points; break;
+			}
+         
+			this.MeshTopology = topo;
+		}
+      
+		public void SetTopology(MeshTopology topo)
+		{
+			this.MeshTopology = topo;
+		}
+		#endregion
+	}
 }
--- lib/Tri/SubdivTriangle.cs
+++ lib/Tri/SubdivTriangle.cs
@@ -1,19 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Computils.Tri
 {
-	public class TriangleDivider : MonoBehaviour
+	public class SubdivTriangle : MonoBehaviour
 	{
 		public enum Methd { Equal, MaxSize };
       
 		public ComputeBufferFacade Source;
 		public ComputeBufferFacade Destination;      
-		public float TrianglesSize = 0.3f;
+		public float SubdivsSize = 0.3f;
 		public Methd Method = Methd.MaxSize;
 		public bool AtStart = true;
 		public bool EachUpdate = true;
+
+		public UnityEvent AfterSubdiv;
 
 		private void Start()
 		{
@@ -33,12 +36,14 @@ namespace Computils.Tri
 			buf.GetData(sourceverts);
 
 			Vector3[] newverts = new Vector3[0];
-			if (this.Method.Equals(Methd.MaxSize)) newverts = MaxSizeSubdivTri.GetSubdivVerts(sourceverts, this.TrianglesSize);
-			if (this.Method.Equals(Methd.Equal)) newverts = EqualSubdivTri.GetSubdivVerts(sourceverts, this.TrianglesSize);
-
+			if (this.Method.Equals(Methd.MaxSize)) newverts = MaxSizeSubdivTri.GetSubdivVerts(sourceverts, this.SubdivsSize);
+			if (this.Method.Equals(Methd.Equal)) newverts = EqualSubdivTri.GetSubdivVerts(sourceverts, this.SubdivsSize);
+         
 			var dest = Destination.GetValid();
 			dest = Populators.Utils.UpdateOrCreate(dest, newverts);
 			Destination.Set(dest);
+
+			this.AfterSubdiv.Invoke();
 		}
     }
 }
