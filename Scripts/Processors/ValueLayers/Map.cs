9232d4b896863183b2b1e7c3ffdea0f37e34604a
mark <mark.korput@gmail.com>
2018-12-08 15:13:50 +0100

    Optional custom source buffer for ValueLayers/Map

--- Scripts/Processors/ValueLayers/Map.cs
+++ Scripts/Processors/ValueLayers/Map.cs
@@ -11,6 +11,7 @@ namespace Computils.Processors.ValueLayers
 			public const string Kernel = "CSMap";
 			public const string ResolutionX = "ResolutionX";
 			public const string dest_buf = "dest_buf";
+			public const string mapSource_buf = "mapSource_buf";
          
 			public const string FromMin = "FromMin";
 			public const string FromMax = "FromMax";
@@ -20,6 +21,8 @@ namespace Computils.Processors.ValueLayers
 		}      
 
 		public ComputeShader Shader;
+        [Tooltip("Optional, by default use the the ComputeBuffer given by the ValuesCollector")]
+		public ComputeBufferFacade SourceValuesFacade;
 		public float FromMin = 0.0f;
 		public float FromMax = 1.0f;
 		public float ToMin = 1.0f;
@@ -27,19 +30,23 @@ namespace Computils.Processors.ValueLayers
 		public bool Clamp = false;
       
 		private ShaderRunner Runner = null;
-      
+
 		public override void Apply(ComputeBuffer values_buf)
 		{
 			if (this.Runner == null) {
 				this.Runner = ShaderRunner.Create(this.Shader, ShaderProps.Kernel, 4, 4, ShaderProps.ResolutionX);
 			}
 
+			var source_buf = this.SourceValuesFacade == null ? null : this.SourceValuesFacade.GetValid();
+			if (source_buf == null) source_buf = values_buf;
+
 			this.Runner.Shader.SetFloat(ShaderProps.FromMin, this.FromMin);
 			this.Runner.Shader.SetFloat(ShaderProps.FromMax, this.FromMax);
 			this.Runner.Shader.SetFloat(ShaderProps.ToMin, this.ToMin);
 			this.Runner.Shader.SetFloat(ShaderProps.ToMax, this.ToMax);
 			this.Runner.Shader.SetFloat(ShaderProps.ToMax, this.ToMax);
-			this.Runner.Shader.SetBool(ShaderProps.Clamp, this.Clamp);         
+			this.Runner.Shader.SetBool(ShaderProps.Clamp, this.Clamp);
+			this.Runner.Shader.SetBuffer(this.Runner.Kernel, ShaderProps.mapSource_buf, source_buf);
 			this.Runner.Run(values_buf, ShaderProps.dest_buf);
 		}
 	}
