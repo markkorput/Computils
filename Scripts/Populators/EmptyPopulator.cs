7a96e78bc8e022bd033f434d9e8e0e3e743e17b5
mark <mark.korput@gmail.com>
2018-12-12 09:39:03 +0100

    new public action method for EmptyPopulator

--- Scripts/Populators/EmptyPopulator.cs
+++ Scripts/Populators/EmptyPopulator.cs
@@ -8,40 +8,47 @@ using System.Collections;
 namespace Computils.Populators
 {
 	[AddComponentMenu("Computils/Populators/Empty Verts Buffer Populator")]
-    class EmptyPopulator : MonoBehaviour
-    {
-        public ComputeBufferFacade Facade;
+	class EmptyPopulator : MonoBehaviour
+	{
+		public ComputeBufferFacade Facade;
 		public ComputeBufferFacade AmountFacade;
 		public int Amount = 10000;
 		public Vector3 StartValue;
-
+      
 		private void OnEnable()
-        {
-            Vector3[] verts = null;
-
-
+		{         
 			if (AmountFacade == null)
 			{
 				this.Populate(this.Amount);
-			} else {
+			}
+			else
+			{
 				this.AmountFacade.GetValidAsync().Then((amountbuf) =>
 				{
 					this.Amount = amountbuf.count;
 					this.Populate(this.Amount);
 				}).Done();
 			}
-        }
+		}
       
-		private static Vector3[] GetVerts(int amount, Vector3 val) {
+		private static Vector3[] GetVerts(int amount, Vector3 val)
+		{
 			Vector3[] verts = new Vector3[amount];
 			for (int i = 0; i < amount; i++) verts[i] = val;
 			return verts;
 		}
-
-		public void Populate(int amount) {
+      
+		public void Populate(int amount)
+		{
 			var verts = GetVerts(amount, this.StartValue);
-            ComputeBuffer buf = Utils.Create(verts);
-            Facade.Set(buf);         
+			ComputeBuffer buf = Utils.UpdateOrCreate(Facade.Get(), verts);
+			Facade.Set(buf);
 		}
-    }
+      
+		#region Public Action Methods
+		public void Repopulate() {
+			this.OnEnable();
+		}
+		#endregion
+	}
 }
